d656b2286720bc5c82da1ba3bab5800df88bec76
Abdus.Sobhan <masiceiu@gmail.com>
2021-01-03 09:15:33 +0600

    Initail Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from './pages/landing/landing.component';
+import {AboutComponent} from './pages/about/about.component';
+import {SupportComponent} from './pages/support/support.component';
+import {ResourcesComponent} from './pages/resources/resources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent  },
+  { path: 'about', component: AboutComponent  },
+  { path: 'support', component: SupportComponent  },
+  { path: 'resource', component: ResourcesComponent  },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: '**', component: ResourcesComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SupportComponent } from './pages/support/support.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HeaderComponent } from './common/header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    SupportComponent,
+    ResourcesComponent,
+    PageNotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
